bdf85710ec4c528b214ba5e8429cf204d07e4824
TimotheeCrouzet <tim@tcrouzet.com>
2024-09-30 12:05:13 +0000

    voici mon travail

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from math import sqrt
+import math
 
 #### Fonction secondaire
-
-
 def isprime(p):
+    """cherche si un nombre est premier
+    Args: 
+        p : entier relatifs
+    Returns:
+        True or False
+    >>> isprime(7)
+    True
+    >>> isprime(4)
+    False
+    >>> isprime(-3)
+    False
+    """
+    if p <= 1:
+        return False
+    if p == 2:
+        return True
+    x = int(math.sqrt(p))  # Calcul de la racine carrée de p
+    for i in range(2, x + 1):
+        if p % i == 0:
+            return False
+    return True
+   
+print(isprime(5))
 
-    # votre code ici
 
-    pass
 
 #### Fonction principale
 
